66500971d8f5043417eab51528278f0d17b64b02
Aniket <annudubey1026@gmail.com>
2023-12-28 12:56:17 +0530

    feat :Implement Database Connectivity

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,11 @@
-console.log("Love your family ");
+import express from 'express';
+import { config } from 'dotenv';
+import { connectDB } from './db/db';
+
+const app = express();
+config();
+connectDB().then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server listening on port ${process.env.PORT} ...`);
+  });
+});
