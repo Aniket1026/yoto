37e8922d3f15865f15d687e26ce30897a7f855f3
Aniket <annudubey1026@gmail.com>
2024-02-13 01:46:30 +0530

    feat: Implement Get Single Playlist Route and Controller

--- server/controllers/playlist.controller.ts
+++ server/controllers/playlist.controller.ts
@@ -4,6 +4,7 @@ import { apiError } from '../utils/apiError';
 import { UserRequest } from '../interfaces/request.interface';
 import { Playlist } from '../models/playlist.model';
 import { apiResponse } from '../utils/apiResponse';
+import mongoose from 'mongoose';
 
 export const createPlaylist = asyncHandler(
   async (req: UserRequest, res: Response) => {
@@ -26,5 +27,55 @@ export const createPlaylist = asyncHandler(
       .json(
         new apiResponse(newPlaylist, 201, 'Playlist created  successfully')
       );
+  }
+);
+
+export const getSinglePlaylist = asyncHandler(
+  async (req: UserRequest, res: Response) => {
+    const { playlistId } = req.params;
+    if (!playlistId) throw new apiError('Playlist ID is required', 400);
+
+    // const playlist = await Playlist.findById(playlistId).populate(
+    //   'video',
+    //   'title thumbnail'
+    // );
+
+    const playlist = await Playlist.aggregate([
+      {
+        $match: {
+          _id: new mongoose.Types.ObjectId(playlistId)
+        }
+      },
+      {
+        $project: {
+          name: 1,
+          description: 1,
+          owner: 1,
+          video: {
+            $cond: {
+              if: {
+                $eq: ['$owner', [new mongoose.Types.ObjectId(req?.user?._id)]]
+              },
+              then: '$video',
+              else: {
+                $filter: {
+                  input: '$video',
+                  as: 'video',
+                  cond: {
+                    $eq: ['$video.isPublished', true]
+                  }
+                }
+              }
+            }
+          },
+          createdAt: 1,
+          updatedAt: 1
+        }
+      }
+    ]);
+
+    if (!playlist) throw new apiError('Playlist not found', 404);
+
+    res.status(200).json(new apiResponse(playlist, 200, 'Playlist retrieved'));
   }
 );
--- server/routes/playlist.route.ts
+++ server/routes/playlist.route.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
 import { verifyJWT } from '../middlewares/auth.middleware';
-import { createPlaylist } from '../controllers/playlist.controller';
+import {
+  createPlaylist,
+  getSinglePlaylist
+} from '../controllers/playlist.controller';
 
 export const router = Router();
 
 router.post('/playlist', verifyJWT, createPlaylist);
+router.get('/playlist/:playlistId', verifyJWT, getSinglePlaylist);
 
 export const playlist = router;
